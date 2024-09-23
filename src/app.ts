8ff586b1d510f6f5031b574e8ac71f775de9f5ef
AHFahim009 <ahfba009@gmail.com>
2024-09-23 22:01:00 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import authGuard from "./middlewares/authGuard";
 const app = express();
 
 const corsOptions = {
-    origin: "*",
+    origin: "https://gadget-e-commerce-fronted-2024.vercel.app",
     credentials: true,
     // methods: ['GET', 'POST', 'PUT', 'DELETE'],
     // allowedHeaders: ['Content-Type', 'Authorization'],
